5a1c5993f0c2c225af438c3f24e2a79d00c0e25c
Mathias007 <mathias.najlepszy@gmail.com>
2022-03-11 22:03:43 +0100

    Create and optimize BankAccount functions.

--- ConsoleApp6/BankAccount.cs
+++ ConsoleApp6/BankAccount.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bank
 {
@@ -14,10 +11,9 @@ namespace Bank
         public UInt32 Number { get; }
         public string Owner { get; }
         public decimal balance;
+        public decimal MaxLoanValue { get; }
 
-        // DOKOŃCZYĆ LOAN
-        public Loan Loan = new Loan();
-
+        public Loan Loan = new Loan(0);
 
         public decimal Balance
         {
@@ -36,52 +32,95 @@ namespace Bank
             }
         }
 
-        public BankAccount(string name, decimal initialBalance)
+        public BankAccount(string name, decimal initialBalance, decimal maxLoanValue)
         {
             this.Owner = name;
             this.Balance = initialBalance;
             this.Number = accountNumberSeed++;
-            Console.WriteLine($"Utworzono nowe konto z saldem: {initialBalance}");
+            this.MaxLoanValue = maxLoanValue;
+            Console.WriteLine($"Utworzono nowe konto z saldem: {initialBalance} i maksymalnym kredytem: {maxLoanValue}");
+        }
 
+        public static void PrimaryAmountController(decimal amount, string communicate = "Operacja niedozwolona - wpisana kwota nie jest dodatnia")
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), communicate);
+            }
+        }
+
+        public static void AdditionalAmountController(decimal amount, decimal maxNumber, string communicate = "Operacja niedozwolona - wpisana kwota jest za duża")
+        {
+            if (amount > maxNumber)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), communicate);
+            }
+        }
+
+        public void AddTransactionToList(decimal amount, DateTime date, string note)
+        {
+            Transaction transactionToAdd = new Transaction(amount, date, note);
+            AllTransactions.Add(transactionToAdd);
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-            Console.WriteLine($"Dokonanie wpłaty: {amount}");
-            if (amount <=0)
+            Console.WriteLine($"Operacja wpłaty kwoty: {amount}");
+            PrimaryAmountController(amount);
+
+            AddTransactionToList(amount, date, note);
+        }
+
+        public void MakeWithdraw(decimal amount, DateTime date, string note)
+        {
+            Console.WriteLine($"Operacja wypłaty kwoty: {amount}");
+            PrimaryAmountController(amount);
+
+            AddTransactionToList(-amount, date, note);
+        }
+
+        public void GiveLoan(decimal amount, DateTime date, string note)
+        {
+            Console.WriteLine($"Operacja udzielenia kredytu w kwocie: {amount}");
+
+            PrimaryAmountController(amount);
+            AdditionalAmountController(amount, MaxLoanValue, "Przekroczyłeś maksymalną kwotę kredytu dla tego konta");
+
+            Loan.LoanState += amount;
+
+            AddTransactionToList(-amount, date, note);
+        }
+
+        public void PayLoan(decimal amount, DateTime date, string note)
+        {
+            Console.WriteLine($"Operacja spłaty kredytu w kwocie: {amount}");
+
+            PrimaryAmountController(amount);
+            AdditionalAmountController(amount, Loan.LoanState, "Podana kwota przekracza wysokość zadłużenia");
+
+            Loan.LoanState -= amount;
+
+            AddTransactionToList(amount, date, note);
+        }
+
+        public void ListTransactions()
+        {
+            Console.WriteLine("Historia rachunku");
+            int transactionCounter = 0;
+            foreach (Transaction singleTransaction in AllTransactions)
             {
-                throw new ArgumentOutOfRangeException(nameof(amount), "Nie można wpłacić ujem nej kwoty");
+                Console.WriteLine($"Operacja numer {transactionCounter} wykonana w dniu {singleTransaction.Date} na kwotę {singleTransaction.Amount} tytułem {singleTransaction.Note}");
+                Console.WriteLine("");
+                transactionCounter++;
             }
-            Transaction deposit = new Transaction(amount, date, note);
-            AllTransactions.Add(deposit);
-        }
-
-
-        // UZUPEŁNIĆ METODY
-        public void MakeWithdraw()
-        {
-
-        }
-
-        public void GiveLoan()
-        {
-
-        }
-
-        public void PayLoan()
-        {
-
-        }
-
-        public void ListTransaction()
-        {
-
         }
 
         public void DisplayInfo()
         {
-
+            Console.WriteLine("Podstawowe informacje o koncie");
+            Console.WriteLine($"Właściciel rachunku: {Owner}");
+            Console.WriteLine($"Stan konta (po odliczeniu kredytów): {Balance}");
+            Console.WriteLine($"Stan kredytu: {Loan.LoanState}");
         }
-
     }
 }
