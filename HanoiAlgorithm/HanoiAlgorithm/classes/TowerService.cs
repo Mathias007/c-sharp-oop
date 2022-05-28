9e17e9efcbe0340133802c84118048dc8311f2a2
Mathias007 <mathias.najlepszy@gmail.com>
2022-05-28 22:33:08 +0200

    Create TowerService abstract class (algorithm implementation).

---
+++ HanoiAlgorithm/HanoiAlgorithm/classes/TowerService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace HanoiAlgorithm
+{
+    abstract class TowerService : TowersPainter, ITowerService
+    {
+        //public static void InitializeStartElements(int elements, Stack<int> startTower)
+        //{
+        //    for (int i = elements; i > 0; i--)
+        //    {
+        //        startTower.Push(i);
+        //    }
+
+        //    // Console.WriteLine(startTower.Count);
+        //}
+
+        public static void HandleHanoiMovement(int elements, int firstStack, int secondStack, int thirdStack, Stack<int>[] towers, string[] towerElement, char stop)
+        {
+            if (elements > 1) HandleHanoiMovement(elements - 1, firstStack, thirdStack, secondStack, towers, towerElement, stop);
+
+            MoveElementBetweenStacks(firstStack, thirdStack, towers, towerElement, elements, stop);
+
+            if (elements > 1) HandleHanoiMovement(elements - 1, secondStack, firstStack, thirdStack, towers, towerElement, stop);              
+        }
+
+        public static void MoveElementBetweenStacks(int startStack, int finalStack, Stack<int>[] towers, string[] towerElement, int elements, char stop)
+        {
+            // Console.Write($"Move element from {(HanoiStack)startStack} into {(HanoiStack)finalStack}");
+
+            int movingElement = towers[startStack].Pop();
+
+            AnimateMovement(towerElement, startStack, finalStack, towers, movingElement);
+
+            towers[finalStack].Push(movingElement);
+
+            if (towers[2].Count < elements) StopProgram(stop.Equals('T'));
+        }
+    }
+}
